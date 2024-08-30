b879e35c66b11ccfe7fdc23e0f55784d818e42c0
Mabsas <139257914+Mabsas@users.noreply.github.com>
2024-08-31 02:46:26 +0600

    Sign UP done

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,13 @@
 const express = require("express");
 const app = express();
+app.use(express.json());
 require("dotenv").config();
 require("./conn/conn");
+const User = require ("./routes/user");
 
 
+//routes
+app.use("/api/v1",User);    //for sigup user
 //creating port
 app.listen(process.env.PORT,() => {
     console.log(`Server Started at port ${process.env.PORT}`);
---
+++ backend/routes/user.js
@@ -0,0 +1,74 @@
+const router = require("express").Router();
+const User = require("../models/user");
+const bcrypt = require("bcrypt");
+
+
+
+
+//Sign Up 
+router.post("/sign-up", async (req, res) => {
+
+    try {
+
+        const { username, email, password, address } = req.body;
+
+
+        //check username length is more than 3
+
+        if (username.length < 4) {
+
+            return res
+                .status(400)
+                .json({ message: "Username length should be greater than 3 " });
+        }
+
+        //check username already exist ?
+
+        const existingUsername = await User.findOne({ username: username });
+        if (existingUsername) {
+
+            return res
+                .status(400)
+                .json({ message: "Username already exists " });
+        }
+
+
+
+        //check email already exist ?
+
+        const existingEmail = await User.findOne({ email: email });
+        if (existingEmail) {
+
+            return res
+                .status(400)
+                .json({ message: "Email already exists " });
+        }
+
+
+        //check password kength 
+        if (password.length <= 5) {
+
+            return res
+                .status(400)
+                .json({ message: "Password's length should be greater than 5" });
+        }
+
+        const hashPass = await bcrypt.hash(password, 10);   // bcrypt for protecting password
+
+        const newUser = new User({
+            username: username,
+            email: email,
+            password: hashPass,
+            address: address
+        });
+        await newUser.save();
+        return res.status(200).json({ message: "SignUp Successfully" });
+
+    } catch (error) {
+
+        res.status(500).json({ message: "Internal server error" });
+    }
+});
+
+
+module.exports = router;
