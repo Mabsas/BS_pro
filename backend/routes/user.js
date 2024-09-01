2e9d05c8cd0df3fe351ffdaddb5f4a9fe077a42c
Mabsas <139257914+Mabsas@users.noreply.github.com>
2024-09-01 11:46:41 +0600

    post,put and get in database

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -2,6 +2,7 @@ const router = require("express").Router();
 const User = require("../models/user");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
+const { authenticateToken } = require("./userAuth");
 
 
 
@@ -115,7 +116,29 @@ router.post("/sign-in", async (req, res) => {
 });
 
 
-//get-user info
-router.get("")
+//get-user-information
+router.get("/get-user-information", authenticateToken, async (req, res) => {
 
+    try {
+
+        const { id } = req.headers;
+        const data = await User.findById(id).select('-password');
+        return res.status(200).json(data);
+    } catch (error) {
+        res.status(500).json({ message: "Internal server error" });
+    }
+});
+
+
+// update address
+router.put("/update-address", authenticateToken, async (req, res) => {
+    try {
+        const { id } = req.headers;
+        const { address } = req.body;
+        await User.findByIdAndUpdate(id, { address: address });
+        return res.status(200).json({ message: "Address updated successfully" });
+    } catch (error) {
+        res.status(500).json({ message: "Internal server error" });
+    }
+});
 module.exports = router;
