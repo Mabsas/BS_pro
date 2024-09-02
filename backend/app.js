e0bf8f82a87d2dd147cff3f48506aa07a4a34d73
Mabsas <139257914+Mabsas@users.noreply.github.com>
2024-09-02 12:34:28 +0600

    add to cart

--- backend/app.js
+++ backend/app.js
@@ -4,13 +4,15 @@ require("dotenv").config();
 require("./conn/conn");
 const User = require("./routes/user");
 const Books = require("./routes/book");
-const Favourite = require("./routes/favourite");
+const Favourites = require("./routes/favourite");
+const Cart = require("./routes/cart");
 app.use(express.json());
 
 //routes
 app.use("/api/v1", User);    //for sigup & signin user
 app.use("/api/v1", Books);
-app.use("/api/v1", Favourite);
+app.use("/api/v1", Favourites);
+app.use("/api/v1", Cart);
 //creating port
 app.listen(process.env.PORT, () => {
     console.log(`Server Started at port ${process.env.PORT}`);
---
+++ backend/routes/cart.js
@@ -0,0 +1,32 @@
+const router = require("express").Router();
+const User = require("../models/user");
+const { authenticateToken } = require("./userAuth");
+
+
+//add to cart
+router.put("/add-to-cart", authenticateToken, async (req, res) => {
+    try {
+        const { bookid,id } = req.headers;
+        const userData = await User.findById(id);
+        const isBookinCart = userData.cart.includes(bookid);
+
+        if(isBookinCart){
+            return res.json({ status: "Success",
+                message: "Book is already in cart",
+             });
+        }
+        await User.findByIdAndUpdate(id,{
+            $push: {cart: bookid},
+        });
+
+        return res.json({ status: "Success",
+            message: "Book added to cart",
+         });
+
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ message: "An error occured" });
+    }
+});
+
+module.exports = router;
