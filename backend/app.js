d90999d9cc67836cb9caf8a9ea7823e8f6cb355d
Mabsas <139257914+Mabsas@users.noreply.github.com>
2024-09-02 11:56:18 +0600

    favourites

--- backend/app.js
+++ backend/app.js
@@ -1,15 +1,16 @@
 const express = require("express");
 const app = express();
-app.use(express.json());
 require("dotenv").config();
 require("./conn/conn");
 const User = require("./routes/user");
 const Books = require("./routes/book");
+const Favourite = require("./routes/favourite");
 app.use(express.json());
 
 //routes
 app.use("/api/v1", User);    //for sigup & signin user
 app.use("/api/v1", Books);
+app.use("/api/v1", Favourite);
 //creating port
 app.listen(process.env.PORT, () => {
     console.log(`Server Started at port ${process.env.PORT}`);
