b8e1134c2e863cfc9d832b70a7bedc47867a3d71
Mabsas <139257914+Mabsas@users.noreply.github.com>
2024-09-02 16:02:32 +0600

    get cart of user

--- backend/routes/cart.js
+++ backend/routes/cart.js
@@ -6,22 +6,24 @@ const { authenticateToken } = require("./userAuth");
 //add to cart
 router.put("/add-to-cart", authenticateToken, async (req, res) => {
     try {
-        const { bookid,id } = req.headers;
+        const { bookid, id } = req.headers;
         const userData = await User.findById(id);
         const isBookinCart = userData.cart.includes(bookid);
 
-        if(isBookinCart){
-            return res.json({ status: "Success",
+        if (isBookinCart) {
+            return res.json({
+                status: "Success",
                 message: "Book is already in cart",
-             });
+            });
         }
-        await User.findByIdAndUpdate(id,{
-            $push: {cart: bookid},
+        await User.findByIdAndUpdate(id, {
+            $push: { cart: bookid },
         });
 
-        return res.json({ status: "Success",
+        return res.json({
+            status: "Success",
             message: "Book added to cart",
-         });
+        });
 
     } catch (error) {
         console.log(error);
@@ -34,14 +36,33 @@ router.put("/remove-from-cart/:bookid", authenticateToken, async (req, res) => {
     try {
         const { bookid } = req.params;
         const { id } = req.headers;
-        await User.findByIdAndUpdate(id,{
-            $pull: {cart: bookid},
+        await User.findByIdAndUpdate(id, {
+            $pull: { cart: bookid },
         });
 
-        return res.json({ status: "Success",
+        return res.json({
+            status: "Success",
             message: "Book removed from cart",
-         });
+        });
 
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ message: "An error occured" });
+    }
+});
+
+//get cart of a particular user
+router.get("/get-user-cart", authenticateToken, async (req, res) => {
+    try {
+        const { id } = req.headers;
+        const userData = await User.findById(id).populate("cart");
+        const cart = userData.cart.reverse();
+
+        return res.json({
+            status: "Success",
+            data : cart,
+        });
+ 
     } catch (error) {
         console.log(error);
         res.status(500).json({ message: "An error occured" });
