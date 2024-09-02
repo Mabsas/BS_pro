eb6bc2e5de894950cd05a02e4271a02be600c3c1
Mabsas <139257914+Mabsas@users.noreply.github.com>
2024-09-03 02:30:08 +0600

    linking pages

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
 
 const Navbar = () => {
     const links = [
@@ -21,32 +22,35 @@ const Navbar = () => {
     ];
     return (
         <div className="flex bg-zinc-800 text-white px-8 py-4  items-center justify-between">
-            <div className="flex items-center">
+            <Link to="/" className="flex items-center">
                 <img
                     className="h-10 me-4"
                     src="https://cdn-icons-png.flaticon.com/512/10719/10719415.png"
                     alt="logo"
                 />
                 <h1 className="text-2xl font-semibold">BoOkshelf</h1>
-            </div>
+            </Link>
             <div className="nav-links-bookshelf flex items-center gap-4">
                 <div className="flex gap-4">
                 {links.map((items, i) => (
-                    <div
+                    <Link to={items.link}
                     className="hover:text-blue-500 transition-all duration-300" 
                     key={i}
                     >
                         {items.title}{" "}
-                        </div>
+                        </Link>
                 ))}
                 </div>
                 <div className="flex gap-4">
-                    <button className="px-4 py-1 border border-blue-500 rounded hover:bg-white hover:text-zinc-800 transition-all duration-300">
+                    <Link 
+                    to="/Login" className="px-4 py-1 border border-blue-500 rounded hover:bg-white hover:text-zinc-800 transition-all duration-300">
                         LogIn
-                        </button>
-                    <button className="px-4 py-1 bg-blue-500 rounded hover:bg-white hover:text-zinc-800 transition-all duration-300">
+                        </Link>
+                    <Link
+                    to="/SignUp
+                    " className="px-4 py-1 bg-blue-500 rounded hover:bg-white hover:text-zinc-800 transition-all duration-300">
                         SignUp
-                        </button>
+                        </Link>
                 </div>
             </div>
         </div>
