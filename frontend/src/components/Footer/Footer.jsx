a58846b819fec2ca78502ab45b39dfd61d7b8d0d
Mabsas <139257914+Mabsas@users.noreply.github.com>
2024-09-02 23:46:53 +0600

    footer

--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -2,7 +2,11 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <div className="bg-zinc-800 text-white px-8 py-4">
+        <h1 className="text-xs font-semibold text-center">
+            &copy;2024,Made by MABSAS,Guide - THE CODE MASTER
+        </h1>
+    </div>
   )
 }
 
