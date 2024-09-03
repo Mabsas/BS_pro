b623f6522f0bef00f3ffa1738cf30aa209895ce6
Mabsas <139257914+Mabsas@users.noreply.github.com>
2024-09-04 03:05:48 +0600

    Signup connected

--- frontend/src/pages/SignUp.jsx
+++ frontend/src/pages/SignUp.jsx
@@ -1,7 +1,33 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import axios from 'axios';
+import React, { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
 
 const SignUp = () => {
+
+  const [Values, setValues] = useState({
+    username: "",
+    email: "",
+    password: "",
+    address: "",
+  });
+  const navigate = useNavigate();
+  const change = (e) => {
+    const { name, value } = e.target;
+    setValues({ ...Values, [name]: value });
+  };
+  const submit = async () => {
+    try {
+      if (Values.username === "" || Values.email === "" || Values.password === "" || Values.address === "") {
+        alert("Fill up all the fields!");
+      } else {
+        const response = await axios.post("http://localhost:1000/api/v1/sign-up", Values);
+        alert(response.data.message);
+        navigate("/LogIn");
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  }
   return (
     <div className="min-h-screen bg-zinc-900 flex items-center justify-center px-4">
       <div className="bg-zinc-800 rounded-lg px-8 py-5 w-full md:w-3/6 lg:w-2/6 shadow-lg">
@@ -9,50 +35,58 @@ const SignUp = () => {
         <div className="space-y-4">
           <div>
             <label htmlFor="username" className="text-zinc-400 block">Username</label>
-            <input 
+            <input
               type="text"
               id="username"
-              className="w-full mt-2 bg-zinc-900 text-zinc-100 p-3 rounded focus:ring-2 focus:ring-blue-500 outline-none transition duration-300" 
+              className="w-full mt-2 bg-zinc-900 text-zinc-100 p-3 rounded focus:ring-2 focus:ring-blue-500 outline-none transition duration-300"
               placeholder="Username"
               name="username"
               required
+              value={Values.username}
+              onChange={change}
             />
           </div>
           <div>
             <label htmlFor="email" className="text-zinc-400 block">Email</label>
-            <input 
+            <input
               type="email"
               id="email"
-              className="w-full mt-2 bg-zinc-900 text-zinc-100 p-3 rounded focus:ring-2 focus:ring-blue-500 outline-none transition duration-300" 
+              className="w-full mt-2 bg-zinc-900 text-zinc-100 p-3 rounded focus:ring-2 focus:ring-blue-500 outline-none transition duration-300"
               placeholder="xyz@gmail.com"
               name="email"
               required
+              value={Values.email}
+              onChange={change}
             />
           </div>
           <div>
             <label htmlFor="password" className="text-zinc-400 block">Password</label>
-            <input 
+            <input
               type="password"
               id="password"
-              className="w-full mt-2 bg-zinc-900 text-zinc-100 p-3 rounded focus:ring-2 focus:ring-blue-500 outline-none transition duration-300" 
+              className="w-full mt-2 bg-zinc-900 text-zinc-100 p-3 rounded focus:ring-2 focus:ring-blue-500 outline-none transition duration-300"
               placeholder="Password"
               name="password"
               required
+              value={Values.password}
+              onChange={change}
             />
           </div>
           <div>
             <label htmlFor="address" className="text-zinc-400 block">Address</label>
-            <textarea 
+            <textarea
               id="address"
               className="w-full mt-2 bg-zinc-900 text-zinc-100 p-3 rounded focus:ring-2 focus:ring-blue-500 outline-none transition duration-300"
-              rows="5" 
+              rows="5"
               placeholder="Address"
               name="address"
               required
+              value={Values.address}
+              onChange={change}
             />
           </div>
           <div>
-            <button className="w-full bg-gradient-to-r from-blue-500 to-blue-600 text-white font-semibold py-2 rounded hover:opacity-90 transition duration-300">
+            <button className="w-full bg-gradient-to-r from-blue-500 to-blue-600 text-white font-semibold py-2 rounded hover:opacity-90 transition duration-300" onClick={submit}>
               Sign Up
             </button>
             <p className="flex mt-4 items-center justify-center text-zinc-200 font-semibold">
