86ba9b83006db4d37467df2aef6eaa42c942f977
Mabsas <139257914+Mabsas@users.noreply.github.com>
2024-09-04 00:52:44 +0600

    Recent & View Books

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,6 +8,7 @@ import Login from './pages/Login';
 import SignUp from './pages/SignUp';
 import Profile from './pages/Profile';
 import Cart from './pages/cart';
+import ViewBookDetails from './components/ViewBookDetails/ViewBookDetails';
 
 
 const App = () => {
@@ -22,6 +23,7 @@ const App = () => {
           <Route path="/profile" element={<Profile />} />
           <Route  path="/SignUp" element={<SignUp />} />
           <Route  path="/Login" element={<Login />} />
+          <Route path="view-book-details/:id" element={<ViewBookDetails />}/>
         </Routes>
         <Footer />
       </Router>
