db43afb40c2b53cc6527dc546ed6d7902303ddd2
Mabsas <139257914+Mabsas@users.noreply.github.com>
2024-09-13 11:10:11 +0600

    css up2

--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div className="bg-zinc-900 text-white px-8 py-4">
+    <div className="bg-gradient-to-b from-zinc-600 via-zinc-700 to-zinc-800 text-white px-8 py-4">
         <h1 className="text-xs font-semibold text-center">
             &copy;2024,Made by MABSAS,Guide - THE CODE MASTER
         </h1>
