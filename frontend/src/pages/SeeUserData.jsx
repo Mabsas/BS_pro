04afaa746c7504cd06ae42dd50a0bb0aaa16d718
nomanbinsafar <130387824+nomanbinsafar@users.noreply.github.com>
2024-09-21 00:56:37 +0600

    bg white fixx

--- frontend/src/pages/SeeUserData.jsx
+++ frontend/src/pages/SeeUserData.jsx
@@ -10,7 +10,7 @@ const SeeUserData = ({ userDivData, userDiv, setuserDiv}) => {
     <>
       <div className={`${userDiv} top-0 left-0 h-screen w-full bg-zinc-800 opacity-80`}></div> 
       <div className={`${userDiv} top-0 left-0 h-screen w-full flex items-center justify-center`}>
-        <div className="bg-white rounded p-4 w-[80%] md:w-[50%] lg:w-[40%] text-zinc-800">
+        <div className="bg-gradient-to-br from-slate-900 via-gray-600 to-zinc-800 rounded p-4 w-[80%] md:w-[50%] lg:w-[40%] text-zinc-800">
           <div className="flex items-center justify-between">
             <h1 className="text-2xl font-semibold">User Information</h1>
             <button onClick={() => setuserDiv("hidden")}>
