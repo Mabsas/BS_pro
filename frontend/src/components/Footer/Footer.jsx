6c71b69cb084f71525a92694f2be208aca9b227b
Mohammad Sahil <139257914+Mabsas@users.noreply.github.com>
2024-09-21 11:12:34 +0600

    Update Footer.jsx

--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -4,7 +4,7 @@ const Footer = () => {
   return (
     <div className="bg-gradient-to-b from-zinc-600 via-zinc-700 to-zinc-800 text-white px-8 py-4">
         <h1 className="text-xs font-semibold text-center">
-            &copy;2024,Made by MABSAS,Guide - THE CODE MASTER
+            &copy;2024,Build for SD 2.2 Project
         </h1>
     </div>
   )
